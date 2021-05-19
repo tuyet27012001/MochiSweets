d3533d17644faad42ba17642b3568baedb1c997e
tuyet27012001 <anhtuyetnjnjljl@gmail.com>
2021-05-19 19:03:37 +0700

    search , page

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -33,16 +33,13 @@ namespace MochiSweets.Controllers
     [HttpGet("/Admin/Index")]
     public IActionResult Index()
     {
-      
       return View();
     }
 
     [HttpPost("/Admin/AddProduct")]
     public IActionResult AddProduct(string productName, List<string> imageUrl, string detail, int quantity, double price, int categoryID)
     {
-      
       ViewBag.listCategory = adminService.GetListCategory();
-      
       List<Image> img = new List<Image>();
       if (productName != null)
       {
@@ -51,37 +48,104 @@ namespace MochiSweets.Controllers
           Image i = new Image();
           i.imageUrl = "/Admin/img/" + item;
           img.Add(i);
-         
-        }
 
-        Boolean x = adminService.AddProductNew(productName, img , detail, quantity, price, categoryID);
+        }
+        Boolean x = adminService.AddProductNew(productName, img, detail, quantity, price, categoryID);
         if (x == true)
         {
           return Redirect("/Admin/Product");
         }
-        
       }
-      else{
-        
-      }
-
       return View();
     }
 
     [HttpGet("/Admin/AddProduct")]
     public IActionResult AddProduct()
     {
-       ViewBag.listCategory = adminService.GetListCategory();
+      ViewBag.listCategory = adminService.GetListCategory();
       return View();
     }
 
+    // [HttpGet("/Admin/Product")]
+    // public IActionResult Product()
+    // {
+
+    //   ViewBag.listProduct = adminService.GetListProduct();
+    //   ViewBag.listImage = adminService.GetListImage();
+    //   return View();
+    // }
+
+    // [HttpGet("/Admin/Product")]
+    // public IActionResult Product(string category, string search, int limit, int skip)
+    // {
+    //   // chưa có ssd à ???
+    //   // e chua
+    //   //.....
+    //   // tự ái out đây
+    //   //e xin loi
+    //   // nghe chưa đây ?
+    //   // bình thường khi m lấy dữ liệu từ client m phải truyền vào page với lại limit đấy là điều chắc chắn 
+    //   // m phải từ cái limit với cái page để tính ra skip, chứ sao lại có một đống kia, mà cái công thức kia cũng sai bỏ mẹ, thay limit đi là tạch đúng chưa
+    //   // giải thích logic đi  t xem nào, hay thêm số liều
+    //   limit = 5;
+    //   // if (adminService.GetListProductSearch(category ,search).Count % limit == 0)
+    //   // {
+    //   ViewBag.total = adminService.GetListProductSearch(category, search).total;
+    //   // }
+    //   // else{
+    //   //   ViewBag.total = adminService.GetListProductSearch(category ,search).Count / limit + 1;
+    //   // }
+    //   // cái lấy total cũng sai, đã phân trang tức là nó lấy chỉ được số lượng bản ghi cho phép, m lấy tổng bản ghi thê snafo được bằng cách này, m phải dùng riêng 1 câu query mới lấy được chứ???
+    //   //nhưng mà thế này e thấy cũng đù, m thử thế này thế nọ xem nào
+    //   // làm chống cháy vớ va vớ vẩn,
+    //   // m limit lấy 5 bản ghi từ database ra xong m lấy cái list đấy đêm count thì nó chỉ ra được 5 cái chứ nó lấy được hơn à
+    //   ViewBag.listProduct = adminService.GetListProductSearch(category, search, limit, skip);
+    //   ViewBag.search = search;
+    //   ViewBag.select = category;
+    //   ViewBag.listImage = adminService.GetListImage();
+    //   ViewBag.listCategory = adminService.GetListCategory();
+
+    //   ViewBag.skip = skip;
+    //   ViewBag.skip1 = skip / limit;
+    //   ViewBag.skip2 = skip / limit + 1;
+    //   ViewBag.skip3 = skip / limit + 2;
+    //   ViewBag.skip4 = skip - limit;
+    //   ViewBag.skip5 = skip + limit;
+    //   // thế t có skip 6 thì sao, skip 7 thì sao, cái này nó phải tự động tính ra chứ sao lại có cái biến bằng tay thế này
+    //   //tại e nghĩ gì viết đấy thấy chạy dkd thì thôi
+    //   // m thì giỏi rồi nhể, hay lắm
+    //   // để e xem lại ạ
+    //   // xem lại của m 
+    //   //t mất niềm tin rồi
+    //   //vâng 
+    //   // vâng???????????????????????????e xem lại xem 
+    //   ViewBag.limit = limit;
+
+    //   return View();
+    // }
+
     [HttpGet("/Admin/Product")]
-    public IActionResult Product()
+    public IActionResult Product(string category, string search, int page)
     {
-      ViewBag.listProduct = adminService.GetListProduct();
+      if (page == 0)
+      {
+        page = 1;
+      }
+      int limit = 5;
+      int num = adminService.GetListProductSearch(category, search).total;
+      ViewBag.total = Math.Ceiling((double)num / limit);
+      ViewBag.listProduct = adminService.GetListProductSearch(category, search, limit, page);
+      ViewBag.search = search;
+      ViewBag.select = category;
       ViewBag.listImage = adminService.GetListImage();
+      ViewBag.listCategory = adminService.GetListCategory();
+
+      ViewBag.previousPage = page - 1;
+      ViewBag.page = page;
+      ViewBag.nextPage = page + 1;
       return View();
     }
+
 
     [HttpGet("/Admin/updateproduct")]
     public IActionResult updateproduct(int productID)
@@ -90,6 +154,49 @@ namespace MochiSweets.Controllers
       ViewBag.listCategory = adminService.GetListCategory();
       ViewBag.product = adminService.getProductByProductID(productID);
       ViewBag.listImage = adminService.GetListImage();
+      return View();
+    }
+
+    [HttpGet("/Admin/deleteProduct")]
+    public IActionResult deleteProduct(int productID)
+    {
+      // int?  productID = HttpContext.Session.GetInt32("productID");
+
+      ViewBag.product = adminService.deleteProduct(productID);
+      return Redirect("/Admin/Product");
+    }
+
+    [HttpGet("/Admin/deleteImage")]
+    public IActionResult deleteImage(int imageID)
+    {
+      // int?  productID = HttpContext.Session.GetInt32("productID");
+
+      ViewBag.product = adminService.deleteImage(imageID);
+      Product pro = adminService.getProductByImagrID(imageID);
+
+      return Redirect("/Admin/updateproduct?productID=" + pro.productID);
+    }
+
+
+
+    [HttpPost("/Admin/updateproduct")]
+    public IActionResult updateproduct(Product p, List<string> imageUrl)
+    {
+      ViewBag.product = adminService.getProductByProductID(p.productID);
+      ViewBag.listImage = adminService.GetListImage();
+      List<Image> img = new List<Image>();
+      foreach (var item in imageUrl)
+      {
+        Image i = new Image();
+        i.imageUrl = "/Admin/img/" + item;
+        img.Add(i);
+      }
+      Boolean y = adminService.AddImage(img, p.productID);
+      Boolean x = adminService.updateProduct(p, p.productID);
+      if (x == true && y == true)
+      {
+        return Redirect("/Admin/Product");
+      }
       return View();
     }
 
--- Services/AdminService.cs
+++ Services/AdminService.cs
@@ -14,7 +14,7 @@ namespace MochiSweets.Services
     {
       this.dbContext = dbContext;
     }
-    public bool AddProductNew(string productName,List<Image> imageUrl, string detail, int quantity, double price, int categoryID)
+    public bool AddProductNew(string productName, List<Image> imageUrl, string detail, int quantity, double price, int categoryID)
     {
       try
       {
@@ -33,7 +33,7 @@ namespace MochiSweets.Services
         int a = 0;
         foreach (var item in lp)
         {
-            a = item.productID;
+          a = item.productID;
         }
         var cate = dbContext.Product.FirstOrDefault();
         // foreach (var item in imageUrl)
@@ -55,6 +55,30 @@ namespace MochiSweets.Services
       }
     }
 
+    public bool AddImage(List<Image> image, int productID)
+    {
+      try
+      {
+        foreach (var item in image)
+        {
+          // Console.WriteLine(item.imageUrl);
+          Image img = new Image();
+          img.imageUrl = item.imageUrl;
+          // Console.WriteLine(img.imageUrl);
+          img.productID = productID;
+          dbContext.Add(img);
+          dbContext.SaveChanges();
+        }
+        return true;
+      }
+      catch (System.Exception ex)
+      {
+        Console.WriteLine("Error : " + ex.Message);
+        return false;
+      }
+    }
+
+
     public List<Category> GetListCategory()
     {
       List<Category> listCategory = new List<Category>();
@@ -65,23 +89,161 @@ namespace MochiSweets.Services
     public List<Product> GetListProduct()
     {
       List<Product> listProduct = new List<Product>();
-      listProduct = dbContext.Product.OrderByDescending(s=>s.productID).ToList();
-      //OrderBy
+      listProduct = dbContext.Product.OrderByDescending(s => s.productID).Where(s => s.product_status == 0 || s.product_status == 1).ToList();
       return listProduct;
     }
+
+    // public List<Product> GetListProduct(int limit, int skip)
+    // {
+    //   List<Product> listProduct = new List<Product>();
+    //   listProduct = dbContext.Product.OrderByDescending(s => s.productID).Where(s=>s.product_status == 0 || s.product_status == 1).Skip(skip).Take(limit).ToList();
+    //   return listProduct;
+    // }
 
     public List<Image> GetListImage()
     {
       List<Image> listImage = new List<Image>();
-      listImage = dbContext.Image.ToList();
+      listImage = dbContext.Image.Where(s => s.imageUrl != null).ToList();
       return listImage;
     }
 
     public Product getProductByProductID(int productID)
     {
       Product product = dbContext.Product.FirstOrDefault(p => p.productID == productID);
-      product.ListImage = dbContext.Image.Where(x => x.productID == productID).ToList();
+      // product.ListImage = dbContext.Image.Where(x => x.productID == productID).ToList();
       return product;
     }
+
+    public Product getProductByImagrID(int imageID)
+    {
+      Image img = dbContext.Image.FirstOrDefault(p => p.imageID == imageID);
+      Product product = dbContext.Product.FirstOrDefault(p => p.productID == img.productID);
+      // product.ListImage = dbContext.Image.Where(x => x.productID == productID).ToList();
+      return product;
+    }
+
+    public bool updateProduct(Product product, int productID)
+    {
+      var product1 = getProductByProductID(productID);
+      if (product1 != null)
+      {
+        product1.productName = product.productName;
+        product1.detail = product.detail;
+        product1.price = product.price;
+        product1.quantity = product.quantity;
+        product1.categoryID = product.categoryID;
+        product1.product_status = product.product_status;
+        dbContext.Update(product1);
+        dbContext.SaveChanges();
+        return true;
+      }
+      return false;
+    }
+
+    public bool deleteProduct(int productID)
+    {
+      var product1 = getProductByProductID(productID);
+      if (product1 != null)
+      {
+        product1.product_status = 2;
+        dbContext.Update(product1);
+        dbContext.SaveChanges();
+        return true;
+      }
+      return false;
+    }
+
+    public bool deleteImage(int imageID)
+    {
+      Image img = dbContext.Image.FirstOrDefault(p => p.imageID == imageID);
+      if (img != null)
+      {
+        img.imageUrl = null;
+        dbContext.Update(img);
+        dbContext.SaveChanges();
+        return true;
+      }
+      return false;
+    }
+
+    public List<Product> GetListProductSearch(string category, string search, int limit, int page)
+    {
+      List<Product> listProduct = new List<Product>();
+      List<Category> listC = new List<Category>();
+      listC = GetListCategory();
+      int id = 0;
+      int skip = page == 0 || page == 1 ? 0 : (limit * (page-1));
+      foreach (var item in listC)
+      {
+        if (item.categoryName.Equals(category))
+        {
+          id = item.categoryID;
+        }
+      }
+      if (id != 0)
+      {
+        listProduct = dbContext.Product.OrderByDescending(s => s.productID).Where(
+             s => (EF.Functions.Like(s.productName, $"%{search}%")
+             || EF.Functions.Like(s.productID, $"%{search}%")) && (s.categoryID == id)
+              && (s.product_status == 0 || s.product_status == 1)).Skip(skip).Take(limit).ToList();
+      }
+      else
+      {
+        listProduct = dbContext.Product.OrderByDescending(s => s.productID).Where(
+                     s => (EF.Functions.Like(s.productName, $"%{search}%")
+                     || EF.Functions.Like(s.productID, $"%{search}%"))
+                      && (s.product_status == 0 || s.product_status == 1)).Skip(skip).Take(limit).ToList();
+      }
+
+      //OrderBy
+      return listProduct;
+    }
+
+    public dynamic GetListProductSearch(string category, string search)
+    {
+      List<Product> listProduct = new List<Product>();
+      List<Category> listC = new List<Category>();
+      listC = GetListCategory();
+      int id = 0;
+      int total;
+      // int total = dbContext.Product.Count();
+
+      foreach (var item in listC)
+      {
+        if (item.categoryName.Equals(category))
+        {
+          id = item.categoryID;
+          Console.WriteLine(id);
+        }
+      }
+      if (id != 0)
+      {
+        listProduct = dbContext.Product.OrderByDescending(s => s.productID).Where(
+               s => (EF.Functions.Like(s.productName, $"%{search}%")
+               || EF.Functions.Like(s.productID, $"%{search}%")) && (s.categoryID == id)
+                && (s.product_status == 0 || s.product_status == 1)).ToList();
+        //OrderBy
+         total = dbContext.Product.Where(s => (EF.Functions.Like(s.productName, $"%{search}%")
+               || EF.Functions.Like(s.productID, $"%{search}%")) && (s.categoryID == id)
+                && (s.product_status == 0 || s.product_status == 1)).Count();
+      }
+      else
+      {
+        listProduct = dbContext.Product.OrderByDescending(s => s.productID).Where(
+                     s => (EF.Functions.Like(s.productName, $"%{search}%")
+                     || EF.Functions.Like(s.productID, $"%{search}%"))
+                      && (s.product_status == 0 || s.product_status == 1)).ToList();
+        total = dbContext.Product.Where(s => (EF.Functions.Like(s.productName, $"%{search}%")
+      || EF.Functions.Like(s.productID, $"%{search}%"))
+      && (s.product_status == 0 || s.product_status == 1)).Count();
+      }
+      
+      return new
+      {
+        listProduct = listProduct.ToList(),
+        total = total,
+      };
+    }
+
   }
 }
